0775a60ed2230463c4b3ff6431e40d5ca20d52b0
John Li (Tet) <tet@tet.moe>
2022-03-29 11:27:05 -0700

    added curvy

--- components/Slides/Slides.tsx
+++ components/Slides/Slides.tsx
@@ -29,7 +29,10 @@ export const SlideMaster = () => {
         setscroll((s) => {
           const slider = document.getElementById(`slide-${s + 1}`);
           if (slider) {
-            slider.scrollIntoView({ block: overrideFocus[`slide-${s + 1}`] ||  "center", behavior: "smooth" });
+            slider.scrollIntoView({
+              block: overrideFocus[`slide-${s + 1}`] || "center",
+              behavior: "smooth",
+            });
             return s + 1;
           }
           return s;
@@ -40,7 +43,10 @@ export const SlideMaster = () => {
           const slider = document.getElementById(`slide-${Math.max(0, s - 1)}`);
           if (s == 1) return s;
           if (!!slider) {
-            slider.scrollIntoView({ block: overrideFocus[`slide-${s - 1}`] ||  "center", behavior: "smooth" });
+            slider.scrollIntoView({
+              block: overrideFocus[`slide-${s - 1}`] || "center",
+              behavior: "smooth",
+            });
             return s - 1;
           }
           return s;
@@ -106,15 +112,101 @@ export const SlideMaster = () => {
         ></path>
       </svg>
       <Slide3 />
+      <svg
+        id="visual"
+        viewBox="0 0 900 200"
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        className={`w-full bottom-0 left-0 pointer-events-none rotate-180 translate-y-1`}
+      >
+        <path
+          d="M0 102L21.5 103.8C43 105.7 86 109.3 128.8 104.3C171.7 99.3 214.3 85.7 257.2 73.2C300 60.7 343 49.3 385.8 44.3C428.7 39.3 471.3 40.7 514.2 42.7C557 44.7 600 47.3 642.8 49.3C685.7 51.3 728.3 52.7 771.2 51.5C814 50.3 857 46.7 878.5 44.8L900 43L900 0L878.5 0C857 0 814 0 771.2 0C728.3 0 685.7 0 642.8 0C600 0 557 0 514.2 0C471.3 0 428.7 0 385.8 0C343 0 300 0 257.2 0C214.3 0 171.7 0 128.8 0C86 0 43 0 21.5 0L0 0Z"
+          fill="rgb(39 39 42)"
+          strokeLinecap="round"
+          strokeLinejoin="miter"
+        ></path>
+      </svg>
       <Slide4 />
+      <svg
+        id="visual"
+        viewBox="0 0 900 200"
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        className={`w-full bottom-0 left-0 pointer-events-none -translate-y-1`}
+      >
+        <path
+          d="M0 102L21.5 103.8C43 105.7 86 109.3 128.8 104.3C171.7 99.3 214.3 85.7 257.2 73.2C300 60.7 343 49.3 385.8 44.3C428.7 39.3 471.3 40.7 514.2 42.7C557 44.7 600 47.3 642.8 49.3C685.7 51.3 728.3 52.7 771.2 51.5C814 50.3 857 46.7 878.5 44.8L900 43L900 0L878.5 0C857 0 814 0 771.2 0C728.3 0 685.7 0 642.8 0C600 0 557 0 514.2 0C471.3 0 428.7 0 385.8 0C343 0 300 0 257.2 0C214.3 0 171.7 0 128.8 0C86 0 43 0 21.5 0L0 0Z"
+          fill="rgb(39 39 42)"
+          strokeLinecap="round"
+          strokeLinejoin="miter"
+        ></path>
+      </svg>
       <Slide5 />
+      <svg
+        id="visual"
+        viewBox="0 0 900 200"
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        className={`w-full bottom-0 left-0 pointer-events-none rotate-180 translate-y-1`}
+      >
+        <path
+          d="M0 102L21.5 103.8C43 105.7 86 109.3 128.8 104.3C171.7 99.3 214.3 85.7 257.2 73.2C300 60.7 343 49.3 385.8 44.3C428.7 39.3 471.3 40.7 514.2 42.7C557 44.7 600 47.3 642.8 49.3C685.7 51.3 728.3 52.7 771.2 51.5C814 50.3 857 46.7 878.5 44.8L900 43L900 0L878.5 0C857 0 814 0 771.2 0C728.3 0 685.7 0 642.8 0C600 0 557 0 514.2 0C471.3 0 428.7 0 385.8 0C343 0 300 0 257.2 0C214.3 0 171.7 0 128.8 0C86 0 43 0 21.5 0L0 0Z"
+          fill="rgb(39 39 42)"
+          strokeLinecap="round"
+          strokeLinejoin="miter"
+        ></path>
+      </svg>
       <Slide6 />
+
       <Slide7 />
+      <svg
+        id="visual"
+        viewBox="0 0 900 200"
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        className={`w-full bottom-0 left-0 pointer-events-none -translate-y-1`}
+      >
+        <path
+          d="M0 102L21.5 103.8C43 105.7 86 109.3 128.8 104.3C171.7 99.3 214.3 85.7 257.2 73.2C300 60.7 343 49.3 385.8 44.3C428.7 39.3 471.3 40.7 514.2 42.7C557 44.7 600 47.3 642.8 49.3C685.7 51.3 728.3 52.7 771.2 51.5C814 50.3 857 46.7 878.5 44.8L900 43L900 0L878.5 0C857 0 814 0 771.2 0C728.3 0 685.7 0 642.8 0C600 0 557 0 514.2 0C471.3 0 428.7 0 385.8 0C343 0 300 0 257.2 0C214.3 0 171.7 0 128.8 0C86 0 43 0 21.5 0L0 0Z"
+          fill="rgb(39 39 42)"
+          strokeLinecap="round"
+          strokeLinejoin="miter"
+        ></path>
+      </svg>
       <Slide8 />
       <Slide9 />
+      <svg
+        id="visual"
+        viewBox="0 0 900 200"
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        className={`w-full bottom-0 left-0 pointer-events-none rotate-180 translate-y-1`}
+      >
+        <path
+          d="M0 102L21.5 103.8C43 105.7 86 109.3 128.8 104.3C171.7 99.3 214.3 85.7 257.2 73.2C300 60.7 343 49.3 385.8 44.3C428.7 39.3 471.3 40.7 514.2 42.7C557 44.7 600 47.3 642.8 49.3C685.7 51.3 728.3 52.7 771.2 51.5C814 50.3 857 46.7 878.5 44.8L900 43L900 0L878.5 0C857 0 814 0 771.2 0C728.3 0 685.7 0 642.8 0C600 0 557 0 514.2 0C471.3 0 428.7 0 385.8 0C343 0 300 0 257.2 0C214.3 0 171.7 0 128.8 0C86 0 43 0 21.5 0L0 0Z"
+          fill="rgb(39 39 42)"
+          strokeLinecap="round"
+          strokeLinejoin="miter"
+        ></path>
+      </svg>
       <Slide10 />
       <Slide11 />
+
       <Slide12 />
+      <svg
+        id="visual"
+        viewBox="0 0 900 200"
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        className={`w-full bottom-0 left-0 pointer-events-none -translate-y-1`}
+      >
+        <path
+          d="M0 102L21.5 103.8C43 105.7 86 109.3 128.8 104.3C171.7 99.3 214.3 85.7 257.2 73.2C300 60.7 343 49.3 385.8 44.3C428.7 39.3 471.3 40.7 514.2 42.7C557 44.7 600 47.3 642.8 49.3C685.7 51.3 728.3 52.7 771.2 51.5C814 50.3 857 46.7 878.5 44.8L900 43L900 0L878.5 0C857 0 814 0 771.2 0C728.3 0 685.7 0 642.8 0C600 0 557 0 514.2 0C471.3 0 428.7 0 385.8 0C343 0 300 0 257.2 0C214.3 0 171.7 0 128.8 0C86 0 43 0 21.5 0L0 0Z"
+          fill="rgb(39 39 42)"
+          strokeLinecap="round"
+          strokeLinejoin="miter"
+        ></path>
+      </svg>
       <Slide13 />
     </div>
   );
